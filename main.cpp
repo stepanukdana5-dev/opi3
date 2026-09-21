43b713e03caa1ed21474df62acd45eb540710477
Vanno2145 <119109120+Vanno2145@users.noreply.github.com>
2026-09-21 13:34:45 +0300

    Refactor add function in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
+
 int add(int a, int b) {
     return a + b;
-}
-
-int main() {
-    int result = add(5, 3);
-    std::cout << "Результат: " << result << std::endl; 
-    return 0;
 }
